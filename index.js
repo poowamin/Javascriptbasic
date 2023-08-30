978d56368518f97bf226024221fd0fbd357c9fee
poowamin <james.poowamin7@gmail.com>
2023-08-30 16:55:29 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function appenImageElem(keyword, index) {
+  const imgElem = document.createElement("img");
+  imgElem.src = `https://source.unsplash.com/400x225/?${keyword}'&sig=${index}`;
+
+  const galleryElem = document.querySelector(".gallery");
+  galleryElem.appendChild(imgElem);
+}
+
+function removePhotos() {
+  const galleryElem = document.querySelector(".gallery");
+  galleryElem.innerHTML = "";
+}
+
+function searchPhotos(event) {
+  const keyword = event.target.value;
+  if (event.key === "Enter" && keyword) {
+    removePhotos();
+    for (let i = 1; i <= 9; i++) {
+      appenImageElem(keyword, i);
+    }
+  }
+}
+
+function run() {
+  const inputElem = document.querySelector("input");
+  inputElem.addEventListener("keydown", searchPhotos);
+}
+
+run();
